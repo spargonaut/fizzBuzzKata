39fb1fb48c5232924f64655cd4e976447668f782
spargonaut <spargonaut@gmail.com>
2018-07-07 01:17:26 -0700

    Add the java implementation for the medium branch

---
+++ java/src/main/java/org/spargonaut/FizzBuzzer.java
@@ -0,0 +1,3 @@
+package org.spargonaut;
+
+public class FizzBuzzer {}
---
+++ java/src/test/java/org/spargonaut/FizzBuzzerTest.java
@@ -0,0 +1,62 @@
+package org.spargonaut;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class FizzBuzzerTest {
+
+    @Test
+    public void shouldHaveAWorkingTestEnvironment() {
+        assertTrue(true);
+    }
+
+//    @Test
+//    public void shouldGenerateAListOfIncreasingNumbers() {
+//        FizzBuzzer fizzBuzzer = new FizzBuzzer();
+//
+//        List<String> generatedNumbers = fizzBuzzer.generate(12);
+//
+//        assertThat(generatedNumbers.get(0), is("1"));
+//        assertThat(generatedNumbers.get(1), is("2"));
+//
+//        assertThat(generatedNumbers.get(3), is("4"));
+//
+//        assertThat(generatedNumbers.get(6), is("7"));
+//        assertThat(generatedNumbers.get(7), is("8"));
+//
+//        assertThat(generatedNumbers.get(10), is("11"));
+//    }
+//
+//    @Test
+//    public void shouldMakeAllNumbersDivisibleByThreeBeFizz() {
+//        FizzBuzzer fizzBuzzer = new FizzBuzzer();
+//
+//        List<String> generatedNumbers = fizzBuzzer.generate(12);
+//
+//        assertThat(generatedNumbers.get(2), is("fizz"));
+//        assertThat(generatedNumbers.get(5), is("fizz"));
+//        assertThat(generatedNumbers.get(8), is("fizz"));
+//        assertThat(generatedNumbers.get(11), is("fizz"));
+//    }
+//
+//    @Test
+//    public void shouldMakeAllNumbersDivisibleByFiveBeBuzz() {
+//        FizzBuzzer fizzBuzzer = new FizzBuzzer();
+//
+//        List<String> generatedNumbers = fizzBuzzer.generate(10);
+//
+//        assertThat(generatedNumbers.get(4), is("buzz"));
+//        assertThat(generatedNumbers.get(9), is("buzz"));
+//    }
+//
+//    @Test
+//    public void shouldMakeAllNumbersDivisibleByFifteenBeFizzBuzz() {
+//        FizzBuzzer fizzBuzzer = new FizzBuzzer();
+//
+//        List<String> generatedNumbers = fizzBuzzer.generate(30);
+//
+//        assertThat(generatedNumbers.get(14), is("fizzbuzz"));
+//        assertThat(generatedNumbers.get(29), is("fizzbuzz"));
+//    }
+}
